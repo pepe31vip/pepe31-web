3e391dc44d1ec9f6049f4ad7cfdf9406a8b7b901
developer <team@pepe31.vip>
2023-07-03 17:59:12 +0300

    changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>PEPE 3.1</title>
+        <link  href="favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
